4fec59b8e2ba5e1583f4c2b404bab2900b55f221
Ritsuki Goto <kawakawaritsuki.dev@gmail.com>
2017-09-03 18:20:50 +0900

    [add] background

--- app.js
+++ app.js
@@ -25,7 +25,11 @@ const server = app.listen(process.env.PORT || config.get('server.port'), functio
 // ==========================================================
 
 app.get("/", function (req, res, next) {
-  res.render("login", {"name":config.get("server.name"),"ver":config.get("server.version")});
+  res.render("login", { "name": config.get("server.name"), "ver": config.get("server.version") });
+});
+
+app.get("/login", function (req, res) {
+  if (req.isAuthenticated()) { res.redirect("/list"); } else { res.redirect("/auth/twitter") }
 });
 
 app.get('/auth/twitter', passport.authenticate('twitter'));
@@ -33,22 +37,84 @@ app.get('/auth/twitter', passport.authenticate('twitter'));
 app.get('/auth/twitter/callback', passport.authenticate('twitter', { successRedirect: '/list', failureRedirect: '/' }));
 
 app.get('/list', function (req, res) {
-  if (!req.hasOwnProperty("user")) { res.redirect("/"); return; }
-  res.render("index", {"name":config.get("server.name"),"ver":config.get("server.version")});
+  if (!req.isAuthenticated()) { res.redirect("/"); return; }
+  res.render("index", { "name": config.get("server.name"), "ver": config.get("server.version") });
 });
 
 app.get('/api/list', function (req, res) {
-  if (!req.hasOwnProperty("user")) { res.redirect("/"); return; }
-  if (req.session.hasOwnProperty("list")) { res.send(req.session.list); return; }
+  if (!req.isAuthenticated()) { res.status(403).send('Loading').end(); return; }
+
+  if (!req.session.list && !req.session.loading) { res.status(404).send('Not loaded').end(); return; } // hasn't list
+  if (!req.query.page) { res.status(400).send('Need query "page"').end(); return; } // not found query page
+  if (isNaN(parseInt(req.query.page)) || parseInt(req.query.page) < 0) { res.status(400).send('Bad query "page"').end(); return; } // bad query page
+  if (Math.ceil(req.user._json.friends_count / 100) <= parseInt(req.query.page)) { res.status(400).send('Out of bounds "page"').end(); return; } // bad query page
+  if (req.session.list.length == 0 || req.session.list.length - 1 < parseInt(req.query.page)) { res.status(400).send('Not loaded page').end(); return; } // bad query page
+
+  res.status(200).send(req.session.list[req.query.page]).end();
+});
+
+//next_cursor
+
+app.get('/api/load', function (req, res) {
+  if (!req.isAuthenticated()) { res.status(403).send('Loading').end(); return; }
+  if (req.session.loading) { res.status(406).send('Loading').end(); return; }
+  if (req.query.force != "true" && req.session.list) { res.status(400).send('Loaded. If you need a reload, you can use a "force" option.').end(); return; }
+
+  req.session.loading = true;
+  req.session.loaded = false;
 
   var client = new twitter({
-    consumer_key: process.env.CONSUMER_KEY,
-    consumer_secret: process.env.CONSUMER_SECRET,
+    consumer_key: config.get('api-keys.twitter.ck'),
+    consumer_secret: config.get('api-keys.twitter.cs'),
     access_token_key: req.user.token,
     access_token_secret: req.user.tokenSecret,
   });
   var cursor = -1;
   var users = [];
+  req.session.list = [];
+  req.session.getLength = 0;
+
+  var getLookup = function (client, users) {
+    var b = users.length, cnt = 100, newArr = [];
+    for (var i = 0; i < Math.ceil(b / cnt); i++) {
+      var j = i * cnt;
+      var p = users.slice(j, j + cnt);
+      newArr.push(p);
+    }
+
+    var process = []
+
+    var i = 0;
+    newArr.forEach(function (element, index, array) {
+      process.push(function (callback) {
+        client.get('users/lookup', { "stringify_ids": true, "user_id": element.join(',') }, function (e, d, r) {
+          req.session.list.push(d);
+          req.session.getLength += d.length;
+          req.session.save();
+
+          console.log("push " + ++i + " " + d.length);
+          callback(e, d);
+        });
+      });
+    });
+
+    async.series(process, function (err, results) {
+      req.session.loading = false;
+      req.session.save();
+      if (err) throw err;
+    });
+  }
+
+  var getIds = function (client, cursor, users, done) {
+    client.get('friends/ids', { "screen_name": "parusy168", "stringify_ids": true, "cursor": cursor }, function (error, data, response) {
+    // client.get('friends/ids', { "stringify_ids": true, "cursor": cursor }, function (error, data, response) {
+      if (!error) {
+        console.log("GET IDS " + data.ids.length);
+        Array.prototype.push.apply(users, data.ids);
+        done(users, data.next_cursor_str);
+      };
+    });
+  };
 
   var done = function (u, c) {
     cursor = c;
@@ -57,118 +123,41 @@ app.get('/api/list', function (req, res) {
 
     if (c == 0) {
       console.log("START LOOKUP");
-      getLookup(client, users, function (usersData) {
-        console.log("RETURN");
-        req.session.list = usersData;
-        res.send(usersData);
-      });
+      getLookup(client, users);
     } else {
       getIds(client, cursor, users, done);
     }
   };
 
   getIds(client, cursor, users, done);
+
+  res.status(200).send('Load start').end();
 });
 
-//next_cursor
-
-function getIds(client, cursor, users, done) {
-  // client.get('friends/ids', { "screen_name":"parusy168","stringify_ids": true, "cursor": cursor }, function (error, data, response) {
-  client.get('friends/ids', { "stringify_ids": true, "cursor": cursor }, function (error, data, response) {
-    if (!error) {
-      console.log("GET IDS");
-      Array.prototype.push.apply(users, data.ids);
-      done(users, data.next_cursor);
-    };
-  });
-};
-
-function getLookup(client, users, done) {
-  var b = users.length, cnt = 100, newArr = [];
-  for (var i = 0; i < Math.ceil(b / cnt); i++) {
-    var j = i * cnt;
-    var p = users.slice(j, j + cnt);
-    newArr.push(p);
-  }
-
-  var process = []
-
-  var i = 0;
-  newArr.forEach(function (element, index, array) {
-    process.push(function (callback) {
-      client.get('users/lookup', { user_id: element.join(',') }, function (e, d, r) {
-        i++;
-        console.log(i + "00");
-        callback(e, d);
-      });
-    });
-  });
-
-  async.series(process, function (err, results) {
-    if (err) throw err;
-    var res = [];
-    results.forEach(function (u) {
-      Array.prototype.push.apply(res, u);
-    });
-    done(res);
-  });
-}
-
-app.get('/api/reload', function (req, res) {
-  if (!req.hasOwnProperty("user")) { res.redirect("/"); return; }
-
-  var client = new twitter({
-    consumer_key: process.env.CONSUMER_KEY,
-    consumer_secret: process.env.CONSUMER_SECRET,
-    access_token_key: req.user.token,
-    access_token_secret: req.user.tokenSecret,
-  });
-  client.get('friends/ids', {}, function (error, data, response) {
-    if (!error) {
-      var b = data.ids.length, cnt = 100, newArr = [];
-      for (var i = 0; i < Math.ceil(b / cnt); i++) {
-        var j = i * cnt;
-        var p = data.ids.slice(j, j + cnt);
-        newArr.push(p);
-      }
-      var process = []
-
-      newArr.forEach(function (element, index, array) {
-        process.push(function (callback) {
-          client.get('users/lookup', { user_id: element.join(',') }, function (e, d, r) {
-            callback(e, d);
-          });
-        });
-      });
-
-      async.series(process, function (err, results) {
-        if (err) throw err;
-        var users = [];
-        results.forEach(function (u) {
-          Array.prototype.push.apply(users, u);
-        });
-        req.session.list = users;
-        res.send(users);
-      });
-    }
-  });
+app.get('/api/get-length', function (req, res) {
+  if (!req.isAuthenticated()) { res.status(403).send('Loading').end(); return; }
+  if (!req.session.hasOwnProperty("list")) { res.status(404).end(); return; }
+  res.send({ "length": req.session.getLength, "pages": req.session.list.length });
 });
 
-
-app.get('/api/user_data', function (req, res) {
-  if (!req.hasOwnProperty("user")) { res.redirect("/"); return; }
+app.get('/api/user-data', function (req, res) {
+  if (!req.isAuthenticated()) { res.status(403).send('Loading').end(); return; }
   res.send(req.user._json);
 });
 
 app.get('/api/csv', function (req, res) {
-  if (!req.hasOwnProperty("user")) { res.redirect("/"); return; }
+  if (!req.isAuthenticated()) { res.status(403).send('Loading').end(); return; }
+  if (req.session.loading) { res.status(406).send('Loading').end(); return; }
   if (!req.session.hasOwnProperty("list")) { res.redirect("/"); return; }
-  var csv = "\u{feff}name,screen_name,id\n";
-  req.session.list.forEach(function (user) {
-    csv = csv + user.name + "," + user.screen_name + "," + user.id + "\r\n";
+
+  var csv = "name,screen_name,id\n";
+  req.session.list.forEach(function (users) {
+    users.forEach(function (user) {
+      csv = csv + user.name + "," + user.screen_name + "," + user.id + "\r\n";
+    });
   });
-  res.setHeader('Content-disposition', 'attachment; filename=follow_list.csv');
-  res.setHeader('Content-type', 'text/csv');
+  // res.setHeader('Content-disposition', 'attachment; filename=follow_list.csv');
+  res.setHeader('Content-type', 'text/csv; charset=utf-8');
   res.send(csv);
 });
 
--- public/main.js
+++ public/main.js
@@ -1,70 +1,130 @@
+var length = 0;
+var nowPages = 1;
+var pages = 0;
+var loadedPages = 0;
+
 window.onload = function () {
-  superagent.get("/api/user_data")
+  superagent.get("/api/user-data")
     .end(function (err, res) {
       if (!!err) { console.log("error"); }
       document.getElementById("user-name").innerHTML = res.body.name;
       document.getElementById("user-screen_name").innerHTML = "@" + res.body.screen_name;
       document.getElementById("user-img").src = res.body.profile_image_url.replace(/_normal/g, "");
-
+      document.getElementById("follow-count").innerText = "フォロー数: " + res.body.friends_count + "件";
+      length = res.body.friends_count;
+      pages = Math.ceil(length / 100);
       // document.getElementById("loading").classList.add("clear");
     });
 
-  superagent.get("/api/list")
-    .retry(0)
-    .timeout({ response: 3600000, deadline: 3600000 })
-    .end(function (err, res) {
-      document.getElementById("count").innerText = "取得件数: " + res.body.length + "件";
-      res.body.forEach(function (user) {
-        var container = document.createElement('div');
-        var box = document.createElement('div');
-        var boxf = document.createElement('div');
-        var img = document.createElement('img');
-
-        img.src = user.profile_image_url;
-        boxf.innerHTML = user.name + ' by <a href="https://twitter.com/' + user.screen_name + '/">@' + user.screen_name + "</a> id:" + user.id;
-        boxf.classList.add("box");
-        boxf.classList.add("overflow-text");
-        boxf.classList.add("text-float");
-        box.appendChild(img);
-        box.classList.add("box");
-
-        container.classList.add("boxContainer")
-        container.appendChild(box);
-        container.appendChild(boxf);
-
-        document.getElementById("text").appendChild(document.createElement('hr'));
-        document.getElementById("text").appendChild(container);
-      });
-      $("#loading").fadeOut();
-    });
+  superagent.get("/api/load").end(null);
+  load(0);
+  superagent.get("/api/get-length").end(loadcheck);
 };
 
-function reload() {
+function next() {
+  if (nowPages >= pages) { alert("これ以上ありません"); return; }
   $("#loading").fadeIn();
-  superagent.get("/api/reload").end(function (err, res) {
-    document.getElementById("count").innerText = "取得件数: " + res.body.length + "件";
+  load(++nowPages - 1);
+  document.getElementById("top-h2").innerText = "▼あなたがフォローしている人たち Page:" + nowPages;
+  document.getElementById("bottom-h2").innerText = "▲あなたがフォローしている人たち Page:" + nowPages;
+}
+
+function back() {
+  if (nowPages <= 0) { alert("これ以上ありません"); return; }
+  $("#loading").fadeIn();
+  load(--nowPages - 1);
+  document.getElementById("top-h2").innerText = "▼あなたがフォローしている人たち Page:" + nowPages;
+  document.getElementById("bottom-h2").innerText = "▲あなたがフォローしている人たち Page:" + nowPages;
+}
+
+function nextb() {
+  if (nowPages >= pages) { alert("これ以上ありません"); return; }
+  $("#loading").fadeIn();
+  load(++nowPages - 1);
+  document.getElementById("top-h2").innerText = "▼あなたがフォローしている人たち Page:" + nowPages;
+  document.getElementById("bottom-h2").innerText = "▲あなたがフォローしている人たち Page:" + nowPages;
+  location.hash = ""; location.hash = "top";
+}
+
+function backb() {
+  if (nowPages <= 0) { alert("これ以上ありません"); return; }
+  $("#loading").fadeIn();
+  load(--nowPages - 1);
+  document.getElementById("top-h2").innerText = "▼あなたがフォローしている人たち Page:" + nowPages;
+  document.getElementById("bottom-h2").innerText = "▲あなたがフォローしている人たち Page:" + nowPages;
+  location.hash = ""; location.hash = "top";
+}
+
+function loadcheck(err, res) {
+  if (err || res.body.length != length) {
+    setTimeout(() => {
+      superagent.get("/api/get-length").end(loadcheck);
+    }, 1000);
+  }
+  if (!err) {
+    document.getElementById("get-count").innerText = "　取得件数: " + res.body.length + "件";
+    loadedPages = res.body.pages;
+    console.log("pages:" + loadedPages);
+  }
+};
+
+function load(page) {
+  var process = function (err, res) {
+    if (err) {
+      setTimeout(() => {
+        superagent.get("/api/list?page=" + page).end(process);
+      }, 1000);
+      return;
+    }
+    document.getElementById("text").innerText = "";
     res.body.forEach(function (user) {
       var container = document.createElement('div');
-      var box = document.createElement('div');
       var boxf = document.createElement('div');
       var img = document.createElement('img');
 
       img.src = user.profile_image_url;
       boxf.innerHTML = user.name + ' by <a href="https://twitter.com/' + user.screen_name + '/">@' + user.screen_name + "</a> id:" + user.id;
-      boxf.classList.add("box");
       boxf.classList.add("overflow-text");
       boxf.classList.add("text-float");
-      box.appendChild(img);
-      box.classList.add("box");
 
       container.classList.add("boxContainer")
-      container.appendChild(box);
+      container.appendChild(img);
       container.appendChild(boxf);
 
       document.getElementById("text").appendChild(document.createElement('hr'));
       document.getElementById("text").appendChild(container);
-    });
-    $("#loading").fadeOut();
-  });
 
+      $("#loading").fadeOut();
+    });
+  };
+  superagent.get("/api/list?page=" + page).end(process);
+}
+
+function reload() {
+  $("#loading").fadeIn();
+  superagent.get("/api/load?force=true").end(function (err, res) {
+    superagent.get("/api/get-length").end(loadcheck);
+  });
+  load(0);
+}
+
+function csv() {
+  $("#loading").fadeIn();
+  superagent.get("/api/csv")
+    .end(function (err, res) {
+      if (err) { alert("取得が終了していません。"); $("#loading").fadeOut(); return; }
+      var data = "data:text/csv;charset=utf-8,\u{feff}" + res.text;
+      if ( ~navigator.userAgent.indexOf("Windows") ) {
+        data = data.replace(/\n/g, "\r\n").replace(/\r\r/g, "\r")
+      }
+
+      console.log(res.text);
+
+      var link = document.createElement("a");
+      link.setAttribute("href", encodeURI(data));
+      link.setAttribute("download", "follow_list.csv");
+      link.click();
+      
+      $("#loading").fadeOut();
+    });
 }
